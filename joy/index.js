8775b134c283c266090b82e493474c67ba18f059
kambrad25 <kamronbradford525@gmail.com>
2025-10-29 08:13:01 -0400

    Add files via upload

--- joy/index.js
+++ joy/index.js
@@ -4,9 +4,10 @@ const { log } = console;
 document.addEventListener("DOMContentLoaded", (e) => {
     const navLogo = [...document.querySelectorAll(".logo > div > h1")];
     const abKeys = [...document.querySelectorAll(".ab > h4")];
+    const bioInfo = [...document.querySelectorAll(".bio-info > div")];
+    
 
     navLogo.forEach((i, idx) => {
-        log (i);
         setTimeout(() => {
             setTimeout(() => {
                 i.style.transform = `translate3d(0px,0%,0px)`;
@@ -28,36 +29,67 @@ document.addEventListener("DOMContentLoaded", (e) => {
     // })
 
     setTimeout(() => {
-    setInterval(() => {
-        counter++;
-        log (counter);
-        if (counter > 1) {
-            counter = 0;
-        }
+        setInterval(() => {
+            counter++;
+            log (counter);
+            if (counter > 2) {
+                counter = 0;
+            }
 
-        if (counter == 0) {
-            counter = 0;
-            abKeys[0].classList.remove("active");
-            abKeys[1].classList.add("active");
-        }
-        if (counter == 1) {
-            abKeys[1].classList.remove("active");
-            abKeys[0].classList.add("active");   
-        }
+            if (counter == 0) {
+                counter = 0;
+                abKeys[0].classList.remove("active");
+                abKeys[1].classList.add("active");
+                abKeys[2].classList.remove("active");
+            }
+            if (counter == 1) {
+                abKeys[0].classList.add("active");
+                abKeys[1].classList.remove("active");
+                abKeys[2].classList.remove("active"); 
+            }
+            if (counter == 2) {
+                abKeys[0].classList.remove("active");
+                abKeys[1].classList.remove("active");
+                abKeys[2].classList.add("active");           
+            }
+            
+            }, 2500);
 
-        // abKeys.map((u) => {
-        //     if (counter == 1) {
-        //         if (u.className.includes("active")) {
-        //             u.classList.remove("active");
-        //         } else {
-        //             u.classList.add("active");
-        //         }
-        //     }
-        // })
-           
-        }, 5000);
+    }, 2000);
 
-    }, 4000);
+
+    
+
+    setTimeout(() => {
+        let bioInfoEleOne = [...bioInfo[0].querySelectorAll("span > p")];
+        let bioInfoEleTwo = [...bioInfo[1].querySelectorAll("span > p")];
+        let bioInfoEleThree = [...bioInfo[2].querySelectorAll("span > p")];
+
+
+
+        bioInfoEleOne.map((i, idx) => {
+            setTimeout(() => {
+                i.style.transform=`translate3d(0px, 0%, 0px)`;
+            }, idx * 250)
+        })
+
+        setTimeout(() => {
+            bioInfoEleTwo.map((i, idx) => {
+            setTimeout(() => {
+                i.style.transform=`translate3d(0px, 0%, 0px)`;
+            }, idx * 250)
+        })
+        }, 1600);
+
+
+        setTimeout(() => {
+            bioInfoEleThree.map((i, idx) => {
+            setTimeout(() => {
+                i.style.transform=`translate3d(0px, 0%, 0px)`;
+            }, idx * 250)
+        })
+        }, 3000);
+    }, 6000);
 
     
 })
