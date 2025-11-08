c5781914ddea40b970ca510672ce32f7f21eb304
kambrad25 <kamronbradford525@gmail.com>
2025-11-08 08:40:34 -0500

    Add files via upload

--- joy/index.js
+++ joy/index.js
@@ -2,6 +2,38 @@
 
 const { log } = console;
 
+if (navigator.userAgent.indexOf("Mozilla") !== -1 || navigator.userAgent.indexOf("Firefox") !== -1) {
+    document.documentElement.innerHTML = "";
+
+    let bodyMsg = "Please use another browser!";
+
+    let createEle = document.createElement("p");
+
+    createEle.textContent = bodyMsg;
+
+    createEle.style.color="black";
+
+
+    document.body.appendChild(createEle);
+
+    const bodyStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent:"center",
+        alignItems: "center",
+        height: "100%"
+    }
+
+    for (let i in bodyStyle) {
+
+        if (i == "height") {
+            document.body.style[i] = document.documentElement.style[i] = bodyStyle[i]
+        }
+
+
+        document.body.style[i] = bodyStyle[i];
+    }
+}
 
 // Initialize Lenis
 const lenis = new Lenis();
@@ -602,4 +634,5 @@ document.addEventListener("DOMContentLoaded", (e) => {
     
     init()
 })
+
 
