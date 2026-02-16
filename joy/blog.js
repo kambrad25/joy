db383baff7671a959711038269c8e56957eaa1a8
kambrad25 <kamronbradford525@gmail.com>
2026-02-15 21:42:00 -0500

    Update blog.js

--- joy/blog.js
+++ joy/blog.js
@@ -856,7 +856,7 @@ function more () {
             if (!e.target.matches(".tbs")) return;
             setTimeout(() => {
                 pos=0;
-            },500)
+            },1000)
             move(1000);
 
 
